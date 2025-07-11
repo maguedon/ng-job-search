aa9928a78f04ff466ab93c7e80a1cef6b8811628
GUEDON Mathilde <mathilde.guedon@soprasteria.com>
2025-07-11 13:34:16 +0200

    fix() : remove sanitizer pipe

--- src/app/job/job.component.ts
+++ src/app/job/job.component.ts
@@ -1,15 +1,13 @@
-import {Component, input, InputSignal, inject, effect} from '@angular/core';
+import {Component, effect, inject, input, InputSignal} from '@angular/core';
 import {JobsService} from "../jobs/jobs.service";
 import {Job} from "../jobs/job.model";
 import {DatePipe, NgOptimizedImage} from "@angular/common";
-import {SafeHtmlPipe} from "../utils/safe-html.pipe";
 
 @Component({
   selector: 'app-job',
   standalone: true,
   imports: [
     NgOptimizedImage,
-    SafeHtmlPipe,
     DatePipe
   ],
   templateUrl: './job.component.html',
